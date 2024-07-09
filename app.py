c2052a392b16ed1fe783ea984cb56f51b03ca9b8
edwardo <pierenriquez@gmail.com>
2024-07-09 17:36:00 -0500

    add app strealit

---
+++ app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import pickle
+import os
+from dotenv import load_dotenv
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
+LOGO_NISIRA = "https://www.nisira.com.pe/images/Logo/logo2.png"
+
+st.set_page_config(
+    page_title="ChatBot",
+    page_icon=LOGO_NISIRA,
+    #layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "Nisira System"
+    }
+)
+#col1, col2= st.columns(2, gap='small')
+#with col1:
+def list_files_in_directory(directory):
+    try:
+        files = os.listdir(directory)
+        return [file for file in files if os.path.isfile(os.path.join(directory, file))]
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+def pdf_text(archivos):
+    string_ =""
+    for pdf in archivos:
+        pdf_reader = PdfReader(f"./source/{pdf}")
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            text = text.replace("..", "")
+        string_= string_+text
+    return string_
+
+texto = pdf_text(list_files_in_directory("./source"))
+
+load_dotenv()
+
+with st.sidebar:
+    st.title('Chat App')    
+    st.markdown('''
+    ## Test 
+    This app is an LLM-powered chatbot built using:
+    - Streamlit
+    - LangChain
+    - OpenAI LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Nisira Systems')
+def main():
+    st.header("Chatbot Nisira 💬")
+    text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+    chunks = text_splitter.split_text(text=texto)
+    embeddings = OpenAIEmbeddings()
+    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+    ##
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        avatar_rol = None if message["role"] == "user" else LOGO_NISIRA
+        with st.chat_message(message["role"],avatar=avatar_rol):
+            st.markdown(message["content"])
+    ##
+    
+    query = st.chat_input("Cual es su consulta?")    
+    if query:
+        docs = VectorStore.similarity_search(query=query, k=3)
+ 
+        llm = OpenAI()#
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=query)
+            print(cb)
+        st.write(response)  
+ 
+        
+
+if __name__ == '__main__':
+    main()
+    
+    

c5512c11cca3f865399916262dcbf1c5761ffc52
edwardo <pierenriquez@gmail.com>
2024-07-09 10:56:32 -0500

    test2

--- app_bot.py
+++ app_bot.py
@@ -26,42 +26,43 @@ st.header('ChatBot')
     
 #with col2:
     #st.image(LOGO_NISIRA,use_column_width = False, width = 80)
-ww = st.text_input("")
-client = OpenAI(api_key=ww)#ww
+ww = st.text_input("",type="password")
+if ww :
+    client = OpenAI(api_key=ww)#ww
 
-# Confiracion del modelo usado
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    # Confiracion del modelo usado
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Iniciando el chat
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # Iniciando el chat
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    avatar_rol = None if message["role"] == "user" else LOGO_NISIRA
-    with st.chat_message(message["role"],avatar=avatar_rol):
-        st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        avatar_rol = None if message["role"] == "user" else LOGO_NISIRA
+        with st.chat_message(message["role"],avatar=avatar_rol):
+            st.markdown(message["content"])
 
 
-if prompt := st.chat_input("Alguna consulta?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-        
-    # Display assistant response in chat message container
-    with st.chat_message("assistant",avatar=LOGO_NISIRA):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    if prompt := st.chat_input("Alguna consulta?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+            
+        # Display assistant response in chat message container
+        with st.chat_message("assistant",avatar=LOGO_NISIRA):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 #st.plotly_chart(fig, theme="streamlit", use_container_width=True)

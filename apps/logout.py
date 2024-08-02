de0bf9c63c047882918c686bd633824c690b5002
edwardo <pierenriquez@gmail.com>
2024-08-02 17:04:43 -0500

    add query agricola 3

--- apps/logout.py
+++ apps/logout.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from apps.login import cookies
+
 
 def logout():
     st.session_state['logged_in'] = False
-    cookies["username"] = ""
-    cookies.save()
     st.rerun()
